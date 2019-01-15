1b19c2f7f13babaf0f64525791eae0a9e1f5ccd8
Robert Young <robert.young.ie@gmail.com>
2019-01-15 14:25:06 +0000

    Basic structure of program created.

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -1,2 +1,46 @@
+import java.util.*;
+
 public class MazeRunner {
+
+    Maze myMap = new Maze;
+
+    public static void main(String[] args) {
+        intro();
+    }
+
+    public static void intro() {
+        // welcome user and print map
+    }
+
+    public static String userMover() {
+        // take in desired direction of move, and check if that direction is valid and possible
+
+        if myMap.canIMoveRight("R"); = true{
+            myMap.MoveRight();
+        }
+
+        if myMap.canIMoveLeft("L"); = true {
+            myMap.MoveLeft();
+        }
+
+        if myMap canIMoveUp("U"); = true {
+            myMap.MoveUp();
+        }
+
+        if myMap.canIMoveDown("D"); = true {
+            myMap.MovedDown();
+        }
+
+        return direction;
+
+    }
+
+    public static void movesMessage(moves) {
+        // print after number of moves
+        // count moves
+    }
+    public static void navigatePit(direction) {
+        // pit stuff
+    }
+    
 }
