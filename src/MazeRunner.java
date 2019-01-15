ed5b6d9231d60a3f367396fadb2deca9650cc6e3
Robert Young <robert.young.ie@gmail.com>
2019-01-15 15:16:10 +0000

    Intro and userMove method complete.

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -2,45 +2,65 @@ import java.util.*;
 
 public class MazeRunner {
 
-    Maze myMap = new Maze;
+    public static Maze myMap = new Maze();
 
     public static void main(String[] args) {
         intro();
+        userMove();
     }
 
     public static void intro() {
         // welcome user and print map
+        System.out.println("Welcome to Maze Runner!");
+        System.out.println("Here is your current position:");
+        myMap.printMap();
     }
 
-    public static String userMover() {
-        // take in desired direction of move, and check if that direction is valid and possible
+    public static String userMove() {
+        Scanner input = new Scanner(System.in);
 
-        if myMap.canIMoveRight("R"); = true{
-            myMap.MoveRight();
+        System.out.println("Where would you like to move? (R, L, U, D): ");
+        String user_choice = input.next();
+
+        while (!user_choice.matches("r|l|u|d|R|L|U|D")) {
+            System.out.println("Please eneter a valid move: (R, L, U, D)");
+            user_choice = input.next();
         }
 
-        if myMap.canIMoveLeft("L"); = true {
-            myMap.MoveLeft();
-        }
-
-        if myMap canIMoveUp("U"); = true {
-            myMap.MoveUp();
-        }
-
-        if myMap.canIMoveDown("D"); = true {
-            myMap.MovedDown();
-        }
-
-        return direction;
-
+        return user_choice;
     }
 
-    public static void movesMessage(moves) {
-        // print after number of moves
-        // count moves
-    }
-    public static void navigatePit(direction) {
-        // pit stuff
-    }
+//    public static String userMover() {
+//        // take in desired direction of move, and check if that direction is valid and possible
+//
+//
+//
+//        if myMap.canIMoveRight("R"); = true {
+//            myMap.MoveRight();
+//        }
+//
+//        if myMap.canIMoveLeft("L"); = true {
+//            myMap.MoveLeft();
+//        }
+//
+//        if myMap.canIMoveUp("U"); = true {
+//            myMap.MoveUp();
+//        }
+//
+//        if myMap.canIMoveDown("D"); = true {
+//            myMap.MovedDown();
+//        }
+//
+//        return direction;
+//
+//    }
+//
+//    public static void movesMessage(moves) {
+//        // print after number of moves
+//        // count moves
+//    }
+//    public static void navigatePit(direction) {
+//        // pit stuff
+//    }
     
 }
